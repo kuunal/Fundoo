50414c25def88018da625f083d4a1b4a1e3d4676
kuunal <kunaldeshmukh2503@gmail.com>
2020-12-03 00:37:58 +0530

    [Kunal] ADD: Model for User Account

---
+++ ModelLayer/Account.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace ModelLayer
+{
+    public class Account
+    {
+        [Key]
+        public int AccountId { get; set; }
+        
+        [Required]
+        [Column(TypeName = "varchar(50)")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(50)")]
+        public string LastName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Email { get; set; }
+        
+        [Required]
+        [MaxLength(32)]
+        [MinLength(8)]
+        public string Password { get; set; }
+        
+        [Required]
+        [Column(TypeName = "datetime2")]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        [Column(TypeName = "BigInt")]
+        public long PhoneNumber { get; set; }
+
+    }
+}
