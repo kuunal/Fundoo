395ed2a3c27b61cc9d8f5e788f2e3f7e178bfc55
kuunal <kunaldeshmukh2503@gmail.com>
2020-12-03 00:37:35 +0530

    [Kunal] ADD: DbContext implementation

---
+++ ModelLayer/FundooDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ModelLayer
+{
+    public class FundooDbContext : DbContext
+    {
+        public FundooDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Account> Accounts { get; set; }
+    }
+}
