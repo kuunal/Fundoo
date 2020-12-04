49f24dc8ecfe499a14407a8a0bd3f877e8da864d
kuunal <kunaldeshmukh2503@gmail.com>
2020-12-04 13:58:38 +0530

    [Kunal] ADD: Interface for notes service

---
+++ BusinessLayer/Interface/INoteService.cs
@@ -0,0 +1,17 @@
+﻿using ModelLayer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Interface
+{
+    public interface INoteService
+    {
+        Task<Note> AddNote();
+        Task<Note> DeleteNote();
+        Task<Note> GetNote(int id);
+        Task<Note> GetNote();
+        Task<Note> UpdateNote();
+    }
+}
