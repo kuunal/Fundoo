dde484b4969f154763df8cf0f060ac2d99c8e729
kuunal <kunaldeshmukh2503@gmail.com>
2020-12-04 13:37:31 +0530

    [Kunal] ADD: Notes entity

---
+++ ModelLayer/Note.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace ModelLayer
+{
+    public class Note
+    {
+        [Key]
+        public int NoteId { get; set; }
+
+        public string Title { get; set; }
+
+        [MinLength(4)]
+        [MaxLength(7)]
+        public string Color { get; set; }
+
+        public DateTime Remainder { get; set; }
+
+        [Column(TypeName = "BIT")]
+        public int IsArchieved { get; set; }
+
+
+        [Column(TypeName = "BIT")]
+        public int IsPin { get; set; }
+
+        public string Description { get; set; }
+        public string Image { get; set; }
+    }
+}
