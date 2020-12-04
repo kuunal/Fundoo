1f3d6fcd6c0607d0b3a6bb08e5c9d152f68f6b0b
kuunal <kunaldeshmukh2503@gmail.com>
2020-12-04 13:59:28 +0530

    [Kunal ]ADD: Inherited IRepository

--- RepositoryLayer/Concrete/NotesRepository.cs
+++ RepositoryLayer/Concrete/NotesRepository.cs
@@ -1,10 +1,37 @@
-﻿using System;
+﻿using ModelLayer;
+using RepositoryLayer.Interface;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace RepositoryLayer.Concrete
 {
-    class Class1
+    class NotesRepository : INotesRepository
     {
+        public Task<Note> AddNote()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Note> DeleteNote()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Note> GetNote(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Note> GetNote()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Note> UpdateNote()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
