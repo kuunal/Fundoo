3fc1e4ee868dae98ecd1cc471e78045874bfac3e
kuunal <kunaldeshmukh2503@gmail.com>
2020-12-04 13:52:25 +0530

    [Kunal] ADD: Interface for notes repository

---
+++ RepositoryLayer/Interface/INotesRepository.cs
@@ -0,0 +1,18 @@
+﻿using ModelLayer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryLayer.Interface
+{
+    interface INotesRepository
+    {
+        Task<Note> AddNote();
+        Task<Note> DeleteNote();
+        Task<Note> GetNote(int id);
+        Task<Note> GetNote();
+        Task<Note> UpdateNote();
+
+    }
+}
