0dce93261b1491d1135e4c4a8c237ecff7a2c6ba
kuunal <kunaldeshmukh2503@gmail.com>
2020-12-04 13:38:00 +0530

    [Kunal] ADD: Label entity for notes

---
+++ ModelLayer/Label.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelLayer
+{
+    public class Label
+    {
+        [Key]
+        public int LabelId { get; set; }
+
+        public int NoteId { get; set; }
+
+        public string Labels { get; set; }
+    }
+}
