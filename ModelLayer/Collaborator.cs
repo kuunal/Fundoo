86c368a8f4b1d09445ab94dde4160f994c2aa589
kuunal <kunaldeshmukh2503@gmail.com>
2020-12-04 13:35:21 +0530

    [Kunal] ADD: Entity for collaborator

---
+++ ModelLayer/Collaborator.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelLayer
+{
+    public class Collaborator
+    {
+        [Key]
+        public int Id { get; set; }
+            
+        public Account AccountId { get; set; }
+
+        public Note NoteId { get; set; }
+    }
+}
