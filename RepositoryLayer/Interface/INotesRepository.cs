8ccf11c7404b2887a0e599ef5b5ed19d9ad7bb41
kuunal <kunaldeshmukh2503@gmail.com>
2020-12-05 21:02:55 +0530

    [Kunal] REFACTOR: Changed signature for getting notes

--- RepositoryLayer/Interface/INotesRepository.cs
+++ RepositoryLayer/Interface/INotesRepository.cs
@@ -11,7 +11,7 @@ namespace RepositoryLayer.Interface
         Task<Note> AddNote(Note note);
         Task<Note> DeleteNote(int id);
         Task<Note> GetNote(int id);
-        Task<Note> GetNote();
+        Task<List<Note>> GetNotes(int userId);
         Task<Note> UpdateNote(int id, Note note);
 
     }
