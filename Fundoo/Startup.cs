6ce9fbabd7ac3066b758ec071920b07a6ef1f979
kuunal <kunaldeshmukh2503@gmail.com>
2020-12-06 01:34:54 +0530

    [Kunal] ADD: Dbcontextpool for reusing db object

--- Fundoo/Startup.cs
+++ Fundoo/Startup.cs
@@ -24,7 +24,7 @@ namespace Fundoo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<FundooDbContext>(options => 
+            services.AddDbContextPool<FundooDbContext>(options => 
                 options.UseSqlServer(Configuration.GetConnectionString("ConnectionString"))
             );
             services.AddScoped<IAccountRepository, AccountRepository>();
