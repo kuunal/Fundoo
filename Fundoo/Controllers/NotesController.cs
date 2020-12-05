4b26cab2ecae285658bdd434787f0efd661b5bfd
kuunal <kunaldeshmukh2503@gmail.com>
2020-12-05 20:28:34 +0530

    [Kunal] ADD: Funciton to add notes in controller

--- Fundoo/Controllers/NotesController.cs
+++ Fundoo/Controllers/NotesController.cs
@@ -1,11 +1,43 @@
-﻿using System;
+﻿using BusinessLayer.Interface;
+using Microsoft.AspNetCore.Mvc;
+using ModelLayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Fundoo.Controllers
 {
-    public class NotesController
+    [ApiController]
+    [Route("[controller]")]
+    public class NotesController : ControllerBase
     {
+        private readonly INotesService _service;
+
+        public NotesController(INotesService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public IActionResult GetNotes()
+        {
+            List<Notes> notes = await _service.GetNote();
+            if (notes == null)
+            {
+                return Ok("No notes created");
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddNote(Note note)
+        {
+            Note addednote = await _service.AddNote(note);
+            if (addednote == null)
+            {
+                return BadRequest();
+            }
+            return Ok();
+        }
     }
 }
