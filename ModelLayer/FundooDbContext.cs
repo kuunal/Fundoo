058cdd974a2771da9365765893749011411fc2d6
kuunal <kunaldeshmukh2503@gmail.com>
2020-12-06 01:37:59 +0530

    [Kunal] ADD: Referencail integrity for foreignkeys

--- ModelLayer/FundooDbContext.cs
+++ ModelLayer/FundooDbContext.cs
@@ -15,19 +15,31 @@ namespace ModelLayer
         {
             modelBuilder.Entity<Account>().HasMany(acc => acc.Collaborators)
                 .WithOne(collaborator => collaborator.Account)
-                .HasForeignKey(Collaborator => Collaborator.AccountId);
+                .HasForeignKey(c=>c.AccountId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
 
 
             modelBuilder.Entity<Note>().HasMany(note => note.Collaborators)
                 .WithOne(collaborator => collaborator.Note)
-                .HasForeignKey(Collaborator => Collaborator.NoteId);
+                .HasForeignKey(Collaborator => Collaborator.NoteId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.NoAction);
 
+            modelBuilder.Entity<Account>().HasMany(n => n.Note)
+                .WithOne(a => a.Account)
+                .HasForeignKey(Note=>Note.AccountId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
 
-            modelBuilder.Entity<Note>().HasOne(n => n.Owner)
-                .WithOne(a => a.Note)
-                .HasForeignKey<Note>(n => n.AccountId);
+            modelBuilder.Entity<Note>()
+                .HasMany(note => note.Labels)
+                .WithOne(label => label.Note)
+                .HasForeignKey(label => label.NoteId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
 
-            modelBuilder.Entity<Collaborator>().HasIndex(fields => new { fields.AccountId, fields.NoteId }).IsUnique();
+            //modelBuilder.Entity<Collaborator>().HasIndex(fields => new { fields.AccountId, fields.NoteId }).IsUnique();
         }
     }
 }
