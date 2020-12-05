fe15045c3cc92493c5ec247e3edbe69226fe0da6
kuunal <kunaldeshmukh2503@gmail.com>
2020-12-05 20:36:04 +0530

    [Kunal] ADD: Entity relationships

--- ModelLayer/FundooDbContext.cs
+++ ModelLayer/FundooDbContext.cs
@@ -11,6 +11,23 @@ namespace ModelLayer
         public DbSet<Note> Notes { get; set; }
         public DbSet<Collaborator> Collaborators { get; set; }
         public DbSet<Label> Labels { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Account>().HasMany(acc => acc.Collaborators)
+                .WithOne(collaborator => collaborator.Account)
+                .HasForeignKey(Collaborator => Collaborator.AccountId);
 
+
+            modelBuilder.Entity<Note>().HasMany(note => note.Collaborators)
+                .WithOne(collaborator => collaborator.Note)
+                .HasForeignKey(Collaborator => Collaborator.NoteId);
+
+
+            modelBuilder.Entity<Note>().HasOne(n => n.Owner)
+                .WithOne(a => a.Note)
+                .HasForeignKey<Note>(n => n.AccountId);
+
+            modelBuilder.Entity<Collaborator>().HasIndex(fields => new { fields.AccountId, fields.NoteId }).IsUnique();
+        }
     }
 }
