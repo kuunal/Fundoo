0ddd36f8a0ce6bf01de3ea9534d9062cf91255f9
kuunal <kunaldeshmukh2503@gmail.com>
2020-12-06 23:24:01 +0530

    [Kunal] ADD: TOken manager implentation for encoding and decoding

---
+++ TokenAuthentication/Concrete/TokenManager.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using ModelLayer;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using TokenAuthentication;
+
+namespace Greeting.TokenAuthentication
+{
+    public class TokenManager : ITokenManager
+    {
+        private JwtSecurityTokenHandler tokenHandler;
+        private byte[] secretKey;
+
+        public TokenManager(IConfiguration configuration)
+        {
+            tokenHandler = new JwtSecurityTokenHandler();
+            secretKey = Encoding.ASCII.GetBytes(configuration.GetSection("JwtSecretKey").Value);
+        }
+
+        public string Encode(Account account)
+        {
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[] { 
+                    new Claim(ClaimTypes.UserData, account.AccountId.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddMinutes(1),
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(secretKey),
+                    SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+
+
+        public ClaimsPrincipal Decode(string token)
+        {
+            var claims = tokenHandler.ValidateToken(token, new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(secretKey),
+                ValidateLifetime = true,
+                ValidateAudience = false,
+                ValidateIssuer = false,
+                ClockSkew = TimeSpan.FromMinutes(30)
+            }, out SecurityToken validatedToken) ;
+            return claims;
+        }
+    }
+}
