72e5bb425dc92ca88aaee6c37424c2079cfa0a59
kuunal <kunaldeshmukh2503@gmail.com>
2020-12-07 02:58:11 +0530

    [Kunal] ADD: Abstraction for Add collaborator

---
+++ RepositoryLayer/Interface/ICollaboratorRepository.cs
@@ -0,0 +1,10 @@
+﻿using ModelLayer;
+using System.Threading.Tasks;
+
+namespace RepositoryLayer.Interface
+{
+    public interface ICollaboratorRepository
+    {
+        Task<Collaborator> AddCollaborator(Collaborator collaborator);
+    }
+}
