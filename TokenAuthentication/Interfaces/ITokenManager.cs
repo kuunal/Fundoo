57a0cbafc1e5ce8dbc1103e94efd5a5d73c31dd4
kuunal <kunaldeshmukh2503@gmail.com>
2020-12-06 23:22:59 +0530

    [Kunal] aDD: Interface for tokenmanager

---
+++ TokenAuthentication/Interfaces/ITokenManager.cs
@@ -0,0 +1,11 @@
+﻿using ModelLayer;
+using System.Security.Claims;
+
+namespace TokenAuthentication
+{
+    public interface ITokenManager
+    {
+        string Encode(Account account);
+        ClaimsPrincipal Decode(string token);
+    }
+}
