ba9bcd0b97c821a7566f8220d0ef33a5b7bb347e
kuunal <kunaldeshmukh2503@gmail.com>
2020-12-07 17:07:38 +0530

    [Kunal] ADD: Adding collaborators through repository

---
+++ BusinessLayer/Concrete/CollaboratorService.cs
@@ -0,0 +1,32 @@
+﻿using BusinessLayer.Interface;
+using ModelLayer;
+using RepositoryLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CollaboratorService : ICollaboratorService
+    {
+        private readonly INotesRepository _noteRepository;
+        private readonly ICollaboratorRepository _collaboratorRepository;
+
+
+        public CollaboratorService(INotesRepository noteRepository, ICollaboratorRepository collaboratorRepository) 
+        {
+            _noteRepository = noteRepository;
+            _collaboratorRepository = collaboratorRepository;
+        }
+        public async Task<Collaborator> AddCollaborator(string email, int userId, Collaborator collaborator)
+        {
+            Note note = await _noteRepository.GetNote(collaborator.NoteId, userId);
+            if (note == null)
+            {
+                return null;
+            }
+            return await _collaboratorRepository.AddCollaborator(email, collaborator);
+        }
+    }
+}
---
+++ BusinessLayer/Interface/ICollaboratorService.cs
@@ -0,0 +1,11 @@
+﻿using ModelLayer;
+using System;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Interface
+{
+    public interface ICollaboratorService
+    {
+        Task<Collaborator> AddCollaborator(string email, int userId, Collaborator collaborator);
+    }
+}
