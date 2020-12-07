08977bcb9319564232ecfc0b3b26627bdf1865fb
kuunal <kunaldeshmukh2503@gmail.com>
2020-12-07 23:15:40 +0530

    [Kunal] ADD: Adding labels to db

---
+++ RepositoryLayer/Concrete/LabelRepository.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ModelLayer;
+using RepositoryLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryLayer.Concrete
+{
+    public class LabelRepository : ILabelRepository
+    {
+        private readonly FundooDbContext _context;
+
+        public LabelRepository(FundooDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Label> AddLabel(Label label, int userId)
+        {
+            Note noteOwner = await _context.Notes.FirstOrDefaultAsync(note => note.AccountId == userId);
+            if (noteOwner == null)
+            {
+                throw new Exception("No such note exists!");
+            }
+            var result = await _context.Labels.AddAsync(label);
+            await _context.SaveChangesAsync();
+            return result.Entity;s
+        }
+    }
+}
---
+++ RepositoryLayer/Interface/ILabelRepository.cs
@@ -0,0 +1,13 @@
+﻿using ModelLayer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryLayer.Interface
+{
+    public interface ILabelRepository
+    {
+        Task<Label> AddLabel(Label label, int userId);
+    }
+}
