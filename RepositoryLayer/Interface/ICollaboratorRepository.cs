3a24e72ae00f86cd1e1ea57d35cb396b1e518a3a
kuunal <kunaldeshmukh2503@gmail.com>
2020-12-07 22:34:07 +0530

    [Kunal] ADD: Abstraction for delete and get collaborations

--- RepositoryLayer/Interface/ICollaboratorRepository.cs
+++ RepositoryLayer/Interface/ICollaboratorRepository.cs
@@ -1,4 +1,5 @@
 ﻿using ModelLayer;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace RepositoryLayer.Interface
@@ -6,5 +7,9 @@ namespace RepositoryLayer.Interface
     public interface ICollaboratorRepository
     {
         Task<Collaborator> AddCollaborator(string email, Collaborator collaborator);
+
+        Task<Collaborator> RemoveCollaborator(int collaboratorId, int userId);
+
+        Task<List<Collaborator>> GetCollaboratorAsync(int userId);
     }
 }
