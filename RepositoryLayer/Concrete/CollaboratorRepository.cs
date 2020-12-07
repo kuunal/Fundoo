7c7afae075aa1febbdd493d1a0a3d1a23baa1e80
kuunal <kunaldeshmukh2503@gmail.com>
2020-12-07 22:34:30 +0530

    [Kunal] ADD: Methods to Delete and GEt collaborations

--- RepositoryLayer/Concrete/CollaboratorRepository.cs
+++ RepositoryLayer/Concrete/CollaboratorRepository.cs
@@ -3,6 +3,7 @@ using ModelLayer;
 using RepositoryLayer.Interface;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -20,16 +21,53 @@ namespace RepositoryLayer.Concrete
 
         public async Task<Collaborator> AddCollaborator(string email, Collaborator collaborator)
         {
-            Collaborator collaboratorWithSameUser = await _context.Collaborators
-                                            .FirstOrDefaultAsync(collaborator=>collaborator.email == email);
-            if (collaboratorWithSameUser != null)
-            {
-                return null;
-            }
+            try { 
+                Collaborator collaboratorWithSameUser = await _context.Collaborators
+                                                .FirstOrDefaultAsync(collaborator=>collaborator.email == email);
+                if (collaboratorWithSameUser != null)
+                {
+                    return null;
+                }
 
-            var result = await _context.Collaborators.AddAsync(collaborator);
-            await _context.SaveChangesAsync();
-            return result.Entity;
+                var result = await _context.Collaborators.AddAsync(collaborator);
+                await _context.SaveChangesAsync();
+                return result.Entity;
+            }
+            catch (Microsoft.EntityFrameworkCore.DbUpdateException)
+            {
+                throw new Exception("No such user or email exists!");
+            }
+        }
+
+        public async Task<Collaborator> RemoveCollaborator(int collaboratorId, int userId)
+        {
+            Collaborator isCollaborator = await _context.Collaborators.FindAsync(collaboratorId);
+            Note noteOwner = await _context.Notes
+                .FirstOrDefaultAsync(note=> note.AccountId == userId 
+                                            && note.NoteId == isCollaborator.NoteId);
+            if (noteOwner == null)
+            {
+                throw new Exception("No such note");
+            }
+            if (isCollaborator == null)
+            {
+                throw new Exception("No such collaborator");
+            }
+            return await Task.Run(async () =>
+            {
+
+                var result = _context.Collaborators.Remove(isCollaborator);
+                await _context.SaveChangesAsync();
+                return result.Entity;
+            });
+            
+        }
+
+        public async Task<List<Collaborator>> GetCollaboratorAsync(int userId)
+        {
+            return await _context.Collaborators
+                .FromSqlRaw("select Id, email, Noteid  from collaborators where NoteId in (select NoteId from Notes where AccountId = {0})", userId)
+                .ToListAsync();
         }
     }
 }
