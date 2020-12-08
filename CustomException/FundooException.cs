c10d42760645632121ce85225dd6c238b5f7b426
kuunal <kunaldeshmukh2503@gmail.com>
2020-12-09 01:34:12 +0530

    [Kunal] ADD: Custom exception class for fundoo

---
+++ CustomException/FundooException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomException
+{
+    public class FundooException : Exception
+    {
+        string message;
+        public FundooException(string message) : base(message)
+        {
+            this.message = message;
+        }
+    }
+}
