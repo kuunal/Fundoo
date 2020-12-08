ff48a071b526432bb0052124056d347dc5728d10
kuunal <kunaldeshmukh2503@gmail.com>
2020-12-08 23:18:51 +0530

    [Kunal] ADD: DTOs for notes

--- ModelLayer/DTOs/NoteDTO/NoteRequestDto.cs
+++ ModelLayer/DTOs/NoteDTO/NoteRequestDto.cs
@@ -1,15 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace ModelLayer.DTOs.NoteDTO
 {
-    public class ResponseNoteDto
+    public class NoteRequestDto
     {
-        public int NoteId { get; set; }
-
         public string Title { get; set; }
 
+        [MinLength(4)]
+        [MaxLength(7)]
         public string Color { get; set; }
 
         public DateTime? Remainder { get; set; }
@@ -21,8 +23,6 @@ namespace ModelLayer.DTOs.NoteDTO
 
         public string Description { get; set; }
         public string Image { get; set; }
-
-        public int AccountId { get; set; }
 
     }
 }
---
+++ ModelLayer/DTOs/NoteDTO/NoteResponseDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelLayer.DTOs.NoteDTO
+{
+    public class NoteResponseDto : NoteRequestDto
+    {
+        public int NoteId { get; set; }
+    }
+}
