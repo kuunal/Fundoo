cb853c15086c6a008d86a1877b6cdf10c499a043
kuunal <kunaldeshmukh2503@gmail.com>
2020-12-08 23:49:39 +0530

    [Kunal] ADD: DTOs for collaborators

---
+++ ModelLayer/DTOs/CollaboratorDTO/CollaboratorRequestDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelLayer.DTOs.CollaboratorDTO
+{
+    public class CollaboratorRequestDto
+    {
+        public string email { get; set; }
+
+        public int NoteId { get; set; }
+    }
+}
---
+++ ModelLayer/DTOs/CollaboratorDTO/CollaboratorResponseDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelLayer.DTOs.CollaboratorDTO
+{
+    public class CollaboratorResponseDto 
+    {
+        public int Id { get; set; }
+        public string email { get; set; }
+        public int NoteId { get; set; }
+    }
+}
