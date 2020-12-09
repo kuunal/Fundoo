43b95d31a4ca73269ccc80eaf652ef9abdbfa116
kuunal <kunaldeshmukh2503@gmail.com>
2020-12-09 10:29:13 +0530

    [Kunal] ADD: Email serivce for fundoo

---
+++ EmailService/EmailSender.cs
@@ -0,0 +1,58 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using System;
+using System.Threading.Tasks;
+
+namespace EmailService
+{
+    public class EmailSender : IEmailSender
+    {
+
+        private EmailConfiguration _config;
+
+        public EmailSender(EmailConfiguration emailConfiguration)
+        {
+            _config = emailConfiguration;
+        }
+
+
+        public async Task SendEmail(Message message)
+        {
+            var email = CreateEmail(message);
+            await Send(email);
+        }
+
+        private async Task Send(MimeMessage email)
+        {
+            using (var client = new SmtpClient())
+            {
+                try
+                {
+                    await client.ConnectAsync(_config.SmtpServer, _config.Port, true);
+                    await client.AuthenticateAsync(_config.UserName, Environment.GetEnvironmentVariable("Password", EnvironmentVariableTarget.User));
+                    client.AuthenticationMechanisms.Remove("X0AUTH2");
+                    await client.SendAsync(email);
+                }
+                catch(Exception ex) { 
+                    throw new Exception(ex.ToString());
+                }
+                finally
+                {
+                    await client.DisconnectAsync(true);
+                    client.Dispose();
+                }
+
+            }
+        }
+
+        public MimeMessage CreateEmail(Message message)
+        {
+            var emailMessage = new MimeMessage();
+            emailMessage.From.Add(new MailboxAddress(_config.From));
+            emailMessage.To.AddRange(message.To);
+            emailMessage.Subject = message.Subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = string.Format(message.Content) };
+            return emailMessage;
+        }
+    }
+}
---
+++ EmailService/Message.cs
@@ -0,0 +1,24 @@
+﻿using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailService
+{
+    public class Message
+    {
+        public List<MailboxAddress> To;
+        public string Subject { get; set; }
+        public string Content { get; set; }
+
+        public Message(IEnumerable<string> to, string subject, string content)
+        {
+            To = new List<MailboxAddress>();
+            To.AddRange(to.Select(user => new MailboxAddress(user)));
+            Subject = subject;
+            Content = content; 
+        }
+    }
+}
