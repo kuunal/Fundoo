95e2620b5519fb57dc6099ad4189c6c9585b27db
kuunal <kunaldeshmukh2503@gmail.com>
2020-12-11 16:14:44 +0530

    [Kunal] ADD: POCO for cache settings

---
+++ Caching/CacheSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Caching
+{
+    public class CacheSettings
+    {
+        public bool IsEnabled { get; set; }
+        public string ConnectionString { get; set; }
+    }
+}
