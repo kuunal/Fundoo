1ca0d18409aa006235f5fee3445272e024225d53
kuunal <kunaldeshmukh2503@gmail.com>
2020-12-11 16:16:24 +0530

    [Kunal] ADD: Methods for caching

---
+++ Caching/ResponseCacheService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caching
+{
+    public class ResponseCacheService : IResponseCacheService
+    {
+        private readonly IDistributedCache _distribitedCache;
+
+        public ResponseCacheService(IDistributedCache distribitedCache)
+        {
+            _distribitedCache = distribitedCache;
+        }
+
+        public async Task CacheResponseAsync(string cacheKey, object response, TimeSpan timeToLive)
+        {
+            if(response == null)
+            {
+                return;
+            }
+            var serializedResponse = JsonConvert.SerializeObject(response);
+            await _distribitedCache.SetStringAsync(cacheKey, serializedResponse, new DistributedCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = timeToLive
+            });
+        }
+
+        public async Task<string> GetCachedResponseAsync(string key)
+        {
+            var cachedResponse = await _distribitedCache.GetStringAsync(key);
+            return string.IsNullOrEmpty(cachedResponse) ? null : cachedResponse;
+        }
+    }
+}
