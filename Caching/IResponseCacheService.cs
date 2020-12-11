96786b9d05bc3341271536b01563829c6f8394cf
kuunal <kunaldeshmukh2503@gmail.com>
2020-12-11 16:15:07 +0530

    [Kunal] ADD: Abstraction for caching service

---
+++ Caching/IResponseCacheService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caching
+{
+    public interface IResponseCacheService
+    {
+        Task<string> GetCachedResponseAsync(string key);
+        Task CacheResponseAsync(string cacheKey, object response, TimeSpan timeToLive);
+    }
+}
