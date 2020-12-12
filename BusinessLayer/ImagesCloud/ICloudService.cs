5d7b52fb7b3118ca6e96d9485baf2a105a9fe6aa
kuunal <kunaldeshmukh2503@gmail.com>
2020-12-12 09:46:10 +0530

    [Kunal] ADD: Abstraction for uploading image to cloud

---
+++ BusinessLayer/ImagesCloud/ICloudService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ImagesCloud
+{
+    public interface ICloudService
+    {
+        Task<string> UpdloadToCloud(IFormFile image, string email);
+    }
+}
