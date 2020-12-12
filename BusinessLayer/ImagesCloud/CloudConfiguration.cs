deb7f9c98230f89ffbecfe3561ac63c498341b0e
kuunal <kunaldeshmukh2503@gmail.com>
2020-12-12 09:44:11 +0530

    [Kunal] ADD: Model class for configurations

---
+++ BusinessLayer/ImagesCloud/CloudConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.ImagesCloud
+{
+    public class CloudConfiguration
+    {
+        public string Name { get; set; }
+        public string ApiKey { get; set; }
+        public string Secret { get; set; }
+    }
+}
