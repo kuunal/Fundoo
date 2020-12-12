78b3e0dc1f578ff4b031ab1e5847b9492419076f
kuunal <kunaldeshmukh2503@gmail.com>
2020-12-12 07:51:14 +0530

    [Kunal] ADD: Seperate file for response messages

--- Fundoo/Utilities/ResponseMessages.cs
+++ Fundoo/Utilities/ResponseMessages.cs
@@ -7,12 +7,17 @@ namespace Fundoo.Utilities
 {
     public class ResponseMessages
     {
-        Dictionary<string, int> ResponseDict = new Dictionary<string, int>()
-        {
-            { "Email already exists", 400 },
-            { "Account created", 201 },
-            { "Account created", 201 },
+        public static readonly string SUCCESS = "Successful";
+        public static readonly string FAILED = "Failed";
+        public static readonly string CREATED = "Created successfully";
+        public static readonly string EMAIL_EXISTS = "Email already exists!";
+        public static readonly string MAIL_SENT = "Mail sent! Please check inbox";
+        public static readonly string NO_COLLABORATOR = "You dont havent added any collaborators";
+        public static readonly string NO_SUCH_COLLABORATOR = "No such collaborator";
+        public static readonly string DELETED = "Deleted successfully";
+        public static readonly string NO_NOTES = "You havent created any notes";
+        public static readonly string NO_SUCH_NOTES = "No such note exists!";
+        public static readonly string UPDATED = "Updated successfully";
 
-        };
     }
 }
