4ce39b26dc304de9a440234cdd0e1089a164fb8f
kuunal <kunaldeshmukh2503@gmail.com>
2020-12-12 09:48:36 +0530

    [Kunal] REFACTOR: Image field to contain URL of image

--- ModelLayer/DTOs/NoteDTO/NoteResponseDto.cs
+++ ModelLayer/DTOs/NoteDTO/NoteResponseDto.cs
@@ -4,8 +4,16 @@ using System.Text;
 
 namespace ModelLayer.DTOs.NoteDTO
 {
-    public class NoteResponseDto : NoteRequestDto
+    public class NoteResponseDto 
     {
         public int NoteId { get; set; }
+        public string Title { get; set; }
+        public string Color { get; set; }
+        public DateTime? Remainder { get; set; }
+        public bool IsArchieved { get; set; } = false;
+        public bool IsPin { get; set; } = false;
+        public string Description { get; set; }
+        public string Image { get; set; }
+
     }
 }
