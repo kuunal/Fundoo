2e0a76c13be9538cae5bab7cfc608cc43bb720db
kuunal <kunaldeshmukh2503@gmail.com>
2020-12-12 09:47:56 +0530

    [Kunal] REFACTOR: Image field to support Images

--- ModelLayer/DTOs/NoteDTO/NoteRequestDto.cs
+++ ModelLayer/DTOs/NoteDTO/NoteRequestDto.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -9,20 +11,19 @@ namespace ModelLayer.DTOs.NoteDTO
     public class NoteRequestDto
     {
         public string Title { get; set; }
-
+        
         [MinLength(4)]
         [MaxLength(7)]
         public string Color { get; set; }
-
+        
         public DateTime? Remainder { get; set; }
-
+        
         public bool IsArchieved { get; set; } = false;
-
-
+        
         public bool IsPin { get; set; } = false;
 
         public string Description { get; set; }
-        public string Image { get; set; }
+        public IFormFile Image { get; set; }
 
     }
 }
