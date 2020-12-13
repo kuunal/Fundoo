e57afc8fa58b12863a190847026bc18240d5a89b
kuunal <kunaldeshmukh2503@gmail.com>
2020-12-13 18:09:17 +0530

    [Kunal] ADD: Seperate file for exception messages

---
+++ BusinessLayer/Exceptions/ExceptionMessages.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Exceptions
+{
+    public class ExceptionMessages
+    {
+        public static readonly string NO_SUCH_NOTE = "No such note!";
+        public static readonly string SELF_COLLABORATE = "Cannot collaborate with self";
+        public static readonly string ALREADY_COLLABORATER = "Already added as collaborator";
+        public static readonly string NO_SUCH_USER = "No such user";
+        public static readonly string NO_SUCH_COLLABORATOR = "No such collaborator";
+        public static readonly string INVALID_CREDENTIALS = "Invalid ID or Password";
+        public static readonly string NO_SUCH_LABEL = "No such label exist!";
+
+    }
+}
