5a788d52672684206e663b1222797f6587cf4ea4
kuunal <kunaldeshmukh2503@gmail.com>
2020-12-13 23:47:28 +0530

    [Kunal] REFACTOR: Jwt encode decode to support muliple secret key

--- TokenAuthentication/Concrete/TokenManager.cs
+++ TokenAuthentication/Concrete/TokenManager.cs
@@ -13,14 +13,22 @@ namespace Greeting.TokenAuthentication
     {
         private JwtSecurityTokenHandler tokenHandler;
         private byte[] secretKey;
+        private int expiryTime;
 
         public TokenManager(IConfiguration configuration)
         {
             tokenHandler = new JwtSecurityTokenHandler();
-            secretKey = Encoding.ASCII.GetBytes(configuration.GetSection("JwtSecretKey").Value);
+            secretKey = Encoding.ASCII.GetBytes(configuration.GetSection("Jwt")["AccessSecretKey"]);
+            expiryTime = Convert.ToInt32(configuration.GetSection("Jwt")["ExpiryTime"]);
+
         }
 
         public string Encode(Account account)
+        {
+            return Encode(account, expiryTime);
+        }
+
+        public string Encode(Account account, int ExpiryTimeInMinutes, byte[] secret=null)
         {
             var tokenDescriptor = new SecurityTokenDescriptor
             {
@@ -28,27 +36,32 @@ namespace Greeting.TokenAuthentication
                     new Claim(ClaimTypes.UserData, account.AccountId.ToString()),
                     new Claim(ClaimTypes.Email, account.Email)  
                 }),
-                Expires = DateTime.UtcNow.AddMinutes(1),
+                Expires = DateTime.UtcNow.AddMinutes(ExpiryTimeInMinutes),
                 SigningCredentials = new SigningCredentials(
-                    new SymmetricSecurityKey(secretKey),
+                    new SymmetricSecurityKey(secret == null ? secretKey : secret),
                     SecurityAlgorithms.HmacSha256Signature)
             };
             var token = tokenHandler.CreateToken(tokenDescriptor);
             return tokenHandler.WriteToken(token);
         }
 
-
         public ClaimsPrincipal Decode(string token)
+        {
+            return Decode(token, secretKey);
+        }
+
+
+        public ClaimsPrincipal Decode(string token, byte[] secret)
         {
             var claims = tokenHandler.ValidateToken(token, new TokenValidationParameters
             {
                 ValidateIssuerSigningKey = true,
-                IssuerSigningKey = new SymmetricSecurityKey(secretKey),
+                IssuerSigningKey = new SymmetricSecurityKey(secret),
                 ValidateLifetime = true,
                 ValidateAudience = false,
                 ValidateIssuer = false,
-                ClockSkew = TimeSpan.FromMinutes(30)
-            }, out SecurityToken validatedToken) ;
+                ClockSkew = TimeSpan.FromMinutes(0)
+            }, out SecurityToken validatedToken);
             return claims;
         }
     }
--- TokenAuthentication/Interfaces/ITokenManager.cs
+++ TokenAuthentication/Interfaces/ITokenManager.cs
@@ -5,7 +5,10 @@ namespace TokenAuthentication
 {
     public interface ITokenManager
     {
+        string Encode(Account account, int JwtExpiryTime, byte[] secret);
         string Encode(Account account);
+
         ClaimsPrincipal Decode(string token);
+        ClaimsPrincipal Decode(string token, byte[] secret);
     }
 }
