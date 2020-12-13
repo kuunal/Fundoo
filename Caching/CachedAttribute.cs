2c64e55acb773178ab6df99f01c24cbe9f9e71f1
kuunal <kunaldeshmukh2503@gmail.com>
2020-12-13 18:14:12 +0530

    [Kunal] ADD: Implementation for write through cache

--- Caching/CachedAttribute.cs
+++ Caching/CachedAttribute.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.DependencyInjection;
+using ModelLayer;
+using ModelLayer.DTOs.NoteDTO;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,7 +15,7 @@ namespace Caching
     public class CachedAttribute : Attribute, IAsyncActionFilter
     {
         private readonly int _timeToLiveSeconds;
-
+        private string key;
         public CachedAttribute(int timeToLiveSeconds)
         {
             _timeToLiveSeconds = timeToLiveSeconds;
@@ -20,16 +24,16 @@ namespace Caching
         public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
         {
             var cacheSettings = context.HttpContext.RequestServices.GetRequiredService<CacheSettings>();
+            key = (string)context.HttpContext.Items["userId"];
             if (!cacheSettings.IsEnabled)
             {
                 await next();
                 return;
             }
-            string key = (string)context.HttpContext.Items["userId"];
             var cachedService = context.HttpContext.RequestServices.GetRequiredService<IResponseCacheService>();
             string cachedResponse = await cachedService.GetCachedResponseAsync(key);
 
-            if(!string.IsNullOrEmpty(cachedResponse))
+            if (!string.IsNullOrEmpty(cachedResponse) && context.HttpContext.Request.Method == "GET")
             {
                 var contentResult = new ContentResult
                 {
@@ -40,14 +44,63 @@ namespace Caching
                 context.Result = contentResult;
                 return;
             }
-            
+
 
             var executedContextResult = await next();
 
-            if(executedContextResult.Result is OkObjectResult okObjectResult)
+            if (executedContextResult.Result is OkObjectResult okObjectResult)
             {
-                await cachedService.CacheResponseAsync(key, okObjectResult.Value, TimeSpan.FromSeconds(_timeToLiveSeconds));
+                string responseData = JsonConvert.SerializeObject(okObjectResult.Value);
+                string[] pathArray = context.HttpContext.Request.Path.Value.Split("/");
+                int? pathParam = null;
+                string cachedData = await cachedService.GetCachedResponseAsync(key);
+                List<NoteResponseDto> currentUserData = JsonConvert.DeserializeObject<List<NoteResponseDto>>(cachedData);
+                if (pathArray[pathArray.Length - 1].All(char.IsDigit))
+                {
+                    pathParam = Convert.ToInt32(pathArray[pathArray.Length - 1]);
+                }
+
+                switch (context.HttpContext.Request.Method)
+                {
+                    case "GET":
+                        var responseDataList = JsonConvert.DeserializeObject<Response<List<NoteResponseDto>>>(responseData).Data;
+                        await cachedService.CacheResponseAsync(key, responseDataList, TimeSpan.FromSeconds(_timeToLiveSeconds));
+                        break;
+                    case "POST":
+                        if (cachedData == null)
+                        {
+                            break;
+                        }
+                        int? noteId = pathParam;
+                        if (noteId == null)
+                        {
+                            var deserializedResponse = JsonConvert.DeserializeObject<Response<NoteResponseDto>>(responseData).Data;
+                            currentUserData.Add(deserializedResponse);
+                        }
+                        else
+                        {
+                            var deserializedResponse = JsonConvert.DeserializeObject<Response<NoteResponseDto>>(responseData).Data;
+                            currentUserData.Where(notes => notes.NoteId == noteId).Select(note => note = deserializedResponse);
+                        }
+                        await cachedService.CacheResponseAsync(key, currentUserData, TimeSpan.FromSeconds(600));
+                        break;
+                    case "DELETE":
+                        noteId = pathParam;
+                        if (cachedData == null)
+                        {
+                            break;
+                        }
+                        if (currentUserData.Remove(currentUserData.FirstOrDefault(note => note.NoteId == noteId)))
+                        {
+                            await cachedService.CacheResponseAsync(key, currentUserData, TimeSpan.FromSeconds(600));
+                        }
+                        break;
+                    default:
+                        throw new Exception("Something went wrong");
+                }
             }
-         }
+
+
+        }
     }
 }
